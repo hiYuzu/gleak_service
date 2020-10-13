ca6d23ce737c283f856337b0f8db948d840a7ecb
xuexianling888 <851467287@qq.com>
2020-10-13 14:15:08 +0800

    modify 3000 port

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const jwtAuth = require("./jwtAuth");
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:8000"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
